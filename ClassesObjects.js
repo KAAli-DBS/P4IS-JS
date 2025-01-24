d24a46b15221fdf30d33262e0d2984200ceabe5c
KAAli-DBS <kanzaali.manzar@dbs.ie>
2025-01-24 11:43:29 +0000

    Classes and Objects

--- ClassesObjects.js
+++ ClassesObjects.js
@@ -2,28 +2,23 @@
 // Making Class
 class Car {
     constructor(brand, model) {
-
         // 'this' refers to the new instance of Car
         this.brand = brand;     
         this.model = model;
     }
-
     // Method
     start() {
         console.log(`${this.brand} ${this.model} is starting...`);
     }
-
     // Another method
     drive(speed) {
         console.log(`${this.brand} ${this.model} is driving at ${speed} km/h.`);
     }
 }
-
 // Making Objects
 const myCar = new Car("Nissan", "Note");
 myCar.start(); 
 myCar.drive(120); 
-
 
 //Example 2
 class BankAccount {
@@ -31,13 +26,11 @@ class BankAccount {
         this.owner = owner;
         this.balance = balance;
     }
-
     deposit(amount) {
         this.balance += amount;
         //Inside methods of the class, this refers to the object that the method was called on (i.e., the current instance of the class).
         console.log(`${this.owner} deposited $${amount}. New balance: $${this.balance}`);
-    }
-
+    
     withdraw(amount) {
         if (amount > this.balance) {
             console.log("Insufficient funds.");
@@ -47,7 +40,6 @@ class BankAccount {
         }
     }
 }
-
 const account = new BankAccount("Customer2025", 1000);
 account.deposit(500); 
 account.withdraw(200); 
@@ -59,7 +51,6 @@ class MathUtils {
     static add(a, b) {
         return a + b;
     }
-
     static multiply(a, b) {
         return a * b;
     }
@@ -70,18 +61,15 @@ console.log(MathUtils.multiply(5, 10));
 //Example 4
 class Counter {
     #count = 0; // Private field
-
     increment() {
         this.#count++;
         console.log(`Count is now: ${this.#count}`);
     }
-
     // Access private field through a public method
     getCount() {
         return this.#count; 
     }
 }
-
 const counter = new Counter();
 counter.increment(); 
 console.log(counter.getCount()); 
